6dbf3d7dcfb702ad3177819051ee5ad05c23f763
zukkun <c0b21127c8@edu.teu.ac.jp>
2022-10-04 16:41:25 +0900

    電卓3

--- Ex02/calc.py
+++ Ex02/calc.py
@@ -1,16 +1,58 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
-root = tk.Tk()
-root.title("電卓")
-root.geometry("300x500")
-def button_func(event):
-    event.widget.config(fg="red")
-root.tkinter.Tk()
-for j in range(3):
-    for i in range(3):
-        button_name = "ボタン(" + str(i) + "," + str(j)+ ")"
+from turtle import clear
 
-        # ボタンのインスタンス作成
-        button = tkinter.Button(app,text=button_name)
-        button.grid(column=i, row=j)
+def click_number(event): # 練習3
+    btn = event.widget
+    num = btn["text"]
+    #tkm.showinfo(f"{num}", f"{num}のボタンが押されました")
+    entry.insert(tk.END, num) # 練習5
+
+def click_equal(event):
+    eqn = entry.get()
+    res = eval(eqn)
+    entry.delete(0, tk.END)
+    entry.insert(tk.END, res) # 練習7
+
+
+root = tk.Tk() # 練習1
+root.geometry("300x700")
+
+button = tk.Button(text='C',command=clear)
+button.place(x=500,y=500,width=10,height=10)
+
+def clear(event):
+    entry.delete(0,tk.END)
+
+
+
+entry = tk.Entry(root, width=10, font=(", 40"), justify="right") # 練習4
+entry.grid(row=0, column=0, columnspan=3)
+
+r, c = 1, 0 # r: 行を表す変数／c：列を表す変数
+numbers = list(range(9, -1, -1)) # 数字だけのリスト
+operators = ["+"]
+operators1 = ["-"]
+operators2 = ["*"]
+operators3 = ["/"]
+
+
+ # 演算子だけのリスト
+for i, num in enumerate(numbers+operators+operators1+operators2+operators3, 1):
+    btn = tk.Button(root, text=f"{num}", font=("", 30), width=4, height=2)
+    btn.bind("<1>", click_number)
+    btn.grid(row=r, column=c)
+    c += 1
+    if i%3 == 0:
+        r += 1
+        c = 0
+
+btn = tk.Button(root, text=f"C", font=("", 30), width=4, height=2)
+btn.bind("<1>", clear)
+btn.grid(row=r+1, column=c-2)
+
+btn = tk.Button(root, text=f"=", font=("", 30), width=4, height=2)
+btn.bind("<1>", click_equal)
+btn.grid(row=r, column=c)
+
 root.mainloop()
