a03fd4521a9d2f19e6425c6dff1a2c86885b7573
zukkun <c0b21127c8@edu.teu.ac.jp>
2022-10-04 16:28:51 +0900

    電卓2

--- Ex02/calc.py
+++ Ex02/calc.py
@@ -3,6 +3,14 @@ import tkinter.messagebox as tkm
 root = tk.Tk()
 root.title("電卓")
 root.geometry("300x500")
+def button_func(event):
+    event.widget.config(fg="red")
+root.tkinter.Tk()
+for j in range(3):
+    for i in range(3):
+        button_name = "ボタン(" + str(i) + "," + str(j)+ ")"
 
- 
+        # ボタンのインスタンス作成
+        button = tkinter.Button(app,text=button_name)
+        button.grid(column=i, row=j)
 root.mainloop()
