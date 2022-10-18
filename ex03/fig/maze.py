7e0b4505a353dd7b24d10c824397d3fe98ea875f
zukkun <c0b21127c8@edu.teu.ac.jp>
2022-10-18 16:51:01 +0900

    こうかとん迷路

--- ex03/fig/maze.py
+++ ex03/fig/maze.py
@@ -1,4 +1,8 @@
+from cgitb import reset
 import tkinter as tk
+from turtle import clear
+import random
+import maze_maker as mm # 練習8
 
 # 練習5
 def key_down(event):
@@ -6,22 +10,61 @@ def key_down(event):
     key = event.keysym
 
 
+
 # 練習6
 def key_up(event):
     global key
     key = ""
 
+def search_left(): #右手法攻略
+    global piece_dir,piece_x,piece_y
+    if frameCount % 10 == 0:
+        for i in range(4):
+            dir = (piece_dir+3+i)%4
+            x = piece_x + cx[dir]
+            y = piece_y + cy[dir]
+            if mm[x][y] == 0 or mm[x][y] == 3:
+                break
+        piece_dir = dir
+        piece_x = x
+        piece_y = y
 
+
+# 練習7
 def main_proc():
+    global mx, my
     global cx, cy
     if key == "Up":
-        cy -= 20
+        my -= 1
+    if key =="w":
+        my -= 1    
     if key == "Down":
-        cy += 20
+        my += 1
+    if key == "s":
+        my += 1
     if key == "Left":
-        cx -= 20
+        mx -= 1
+    if key == "a":
+        mx -= 1
     if key == "Right":
-        cx += 20
+        mx += 1
+    if key == "d":
+        mx +=1
+    if key == "s": #ｓキーを押したら開始
+        search_left
+    if key == "r":
+        reset
+    if maze_lst[my][mx] == 0: # 床なら
+        cx, cy = mx*100+50, my*100+50
+    else: # 壁なら
+        if key == "Up":
+            my += 1
+        if key == "Down":
+            my -= 1
+        if key == "Left":
+            mx += 1
+        if key == "Right":
+            mx -= 1
     canv.coords("tori", cx, cy)
     root.after(100, main_proc)
 
@@ -34,9 +77,21 @@ if __name__ == "__main__":
     canv = tk.Canvas(root, width=1500, height=900, bg="black")
     canv.pack()
 
+    # 練習9,10
+    maze_lst = mm.make_maze(15, 9)
+    #print(len(maze_lst)) 9x15
+
+    # print(maze_lst) # 1:壁／0:床
+    mm.show_maze(canv, maze_lst) 
+
+    #if mm[cx][cy] ==2:
+        #canv.delete("all")
+        #canv.create_text(160,160,text="gool",font=(None),fill="red")
+
     # 練習3
-    tori = tk.PhotoImage(file="fig/5.png") 
-    cx, cy = 300, 400
+    tori = tk.PhotoImage(file="fig/11.png") 
+    mx, my = 1, 1
+    cx, cy = mx*100+50, my*100+50
     canv.create_image(cx, cy, image=tori, tag="tori")
 
     # 練習4
