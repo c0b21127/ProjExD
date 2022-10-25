dcfd5d271d7c8e76e2e47318f6d0ce8e6fc3fb41
zukkun <c0b21127c8@edu.teu.ac.jp>
2022-10-25 16:36:23 +0900

    逃げろこうかとん

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,36 +1,95 @@
 import pygame as pg
 import sys
+from random import randint
+
+#def gameover():
+    
+
+def check_bound(obj_rct, scr_rct):
+    """
+    obj_rct：こうかとんrct，または，爆弾rct
+    scr_rct：スクリーンrct
+    領域内：+1／領域外：-1
+    """
+    yoko, tate = +1, +1
+    if obj_rct.left < scr_rct.left or scr_rct.right < obj_rct.right: 
+        yoko = -1
+    if obj_rct.top < scr_rct.top or scr_rct.bottom < obj_rct.bottom: 
+        tate = -1
+    return yoko, tate
+
 
 def main():
-    pg.display.set_caption("逃げろ‼こうかとん‼")
-    scrn_sfc = pg.display.set_mode((1600,900))
-
+    # 練習1
+    pg.display.set_caption("逃げろ！こうかとん")
+    scrn_sfc = pg.display.set_mode((1600, 900))
+    scrn_rct = scrn_sfc.get_rect()
     bg_sfc = pg.image.load("fig/pg_bg.jpg")
     bg_rct = bg_sfc.get_rect()
 
-    tori_sfc = pg.image.load("fig/6.png")
-    tori_sfc = pg.transform.rotozoom(tori_sfc,0,2.0)
+    # 練習3
+    tori_sfc = pg.image.load("fig/11.png")
+    tori_sfc = pg.transform.rotozoom(tori_sfc, 0, 2.0)
     tori_rct = tori_sfc.get_rect()
     tori_rct.center = 900, 400
 
-    clock = pg.time.Clock() #練習１
-    while True:
-        scrn_sfc.blit(bg_sfc,bg_rct) #練習2
-        clock.tick(0.5)
+    # 練習5
+    bomb_sfc = pg.Surface((20, 20)) # 空のSurface
+    bomb_sfc.set_colorkey((0, 0, 0)) # 四隅の黒い部分を透過させる
+    pg.draw.circle(bomb_sfc, (255, 0, 0), (10, 10), 10) # 円を描く
+    bomb_rct = bomb_sfc.get_rect()
+    bomb_rct.centerx = randint(0, scrn_rct.width)
+    bomb_rct.centery = randint(0, scrn_rct.height)
 
-        for event in pg.event.get():
+    # 練習6
+    vx, vy = +3, +3
+
+    clock = pg.time.Clock() # 練習1
+    while True:
+        scrn_sfc.blit(bg_sfc, bg_rct) # 練習2
+        
+        for event in pg.event.get(): # 練習2
             if event.type == pg.QUIT:
                 return
 
-        scrn_sfc.blit(tori_sfc,tori_rct)#練習3 
+        key_states = pg.key.get_pressed()
+        if key_states[pg.K_UP]:    tori_rct.centery -= 2
+        if key_states[pg.K_DOWN]:  tori_rct.centery += 2
+        if key_states[pg.K_LEFT]:  tori_rct.centerx -= 2
+        if key_states[pg.K_RIGHT]: tori_rct.centerx += 2
+        if key_states[pg.K_SPACE]: vx, vy = -10,-10 #スピードが変化する
+        yoko, tate = check_bound(tori_rct, scrn_rct)
+        if yoko == -1:
+            if key_states[pg.K_LEFT]: 
+                tori_rct.centerx += 1
+            if key_states[pg.K_RIGHT]:
+                tori_rct.centerx -= 1
+        if tate == -1:
+            if key_states[pg.K_UP]: 
+                tori_rct.centery += 1
+            if key_states[pg.K_DOWN]:
+                tori_rct.centery -= 1            
+        scrn_sfc.blit(tori_sfc, tori_rct) # 練習3
 
-        pg.display.update() #練習２
+        # 連取7
+        yoko, tate = check_bound(bomb_rct, scrn_rct)
+        vx *= yoko
+        vy *= tate
+        bomb_rct.move_ip(vx, vy) # 練習6
+        scrn_sfc.blit(bomb_sfc, bomb_rct) # 練習5
+
+        # 練習8
+        if tori_rct.colliderect(bomb_rct): # こうかとんrctが爆弾rctと重なったら
+            #def gameover():
+                return 
+                
+
+        
+        pg.display.update() #練習2
         clock.tick(1000)
 
-
-
 if __name__ == "__main__":
-    pg.init() #初期化
-    main() 
-    pg.quit() #初期化の解除
+    pg.init() # 初期化
+    main() # ゲームの本体
+    pg.quit() # 初期化の解除
     sys.exit()
